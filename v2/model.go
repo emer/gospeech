36ca3c97920bad90b5d76157e9adb9829990183f
rohrlich <john.rohrlich@colorado.edu>
2020-04-01 20:38:34 -0700

    Need xml loading code and fix call to rule.Eval()

--- v2/model.go
+++ v2/model.go
@@ -27,65 +27,61 @@
 
 package v2
 
+import "errors"
+
 type Model struct {
 	Categories       []Category
 	Params           []Param
-	Codes            []Code
+	Symbols          []Symbol
 	Postures         []Posture
 	Rules            []Rule
-	EquationGrps     []EquationGrp
-	TransitionGrps   []TransitionGrp
-	TransitionGrpsSp []TransitionGrp
-	Formula          Formula
+	EquationGrps     []EqGroup
+	TransitionGrps   []TransGroup
+	TransitionGrpsSp []TransGroup
+	Formula          *FormulaSymbolList
 }
 
 // Reset
 func (mdl *Model) Reset() {
 	mdl.Categories = mdl.Categories[:0]
 	mdl.Params = mdl.Params[:0]
-	//mdl.Codes = mdl.Codes[:0]
-	//mdl.Postures = mdl.Postures[:0]
-	//mdl.Rules = mdl.Rules[:0]
-	//mdl.EquationGrps = mdl.EquationGrps[:0]
-	//mdl.TransitionGrps = mdl.TransitionGrps[:0]
-	//mdl.TransitionGrpsSp = mdl.TransitionGrpsSp[:0]
+	mdl.Symbols = mdl.Symbols[:0]
+	mdl.Postures = mdl.Postures[:0]
+	mdl.Rules = mdl.Rules[:0]
+	mdl.EquationGrps = mdl.EquationGrps[:0]
+	mdl.TransitionGrps = mdl.TransitionGrps[:0]
+	mdl.TransitionGrpsSp = mdl.TransitionGrpsSp[:0]
 	mdl.Formula.Clear()
 }
 
-//// Load
-//func (mdl *Model) Load(configDir, configFile string) {
-////    Reset()
-//// 	fp := configDir + configFile
-////
-//// 	LOG_DEBUG("Loading xml configuration: " << fp)
-//// 	XMLConfigFileReader cfg(*this, fp);
-//// 	cfg.loadModel();
-//}
-//
-//// Save
-//func (mdl *Model) Save(configDir, configFile string) {
-//	fp := configDir + configFile;
-//
-//	// LOG_DEBUG("Saving xml configuration: " << fp);
-//	// XMLConfigFileWriter cfg(*this, fp);
-//	cfg.saveModel();
-//}
-//
-////
-//// func (mdl *Model) PrintInfo() const {
-//// 	no way I am going to port this code - develop print code as needed
-//// }
-//
-//// ParamIdx returns the index or -1 if not found
-//func (mdl *Model) ParamIdx(nm string) int {
-//	for i, p := range mdl.Params {
-//		if p.Name == nm {
-//			return i
-//		}
-//	}
-//	return -1
-//}
-//
+// Load
+func (mdl *Model) Load(configDir, configFile string) {
+	//    Reset()
+	// 	fp := configDir + configFile
+	//
+	// 	LOG_DEBUG("Loading xml configuration: " << fp)
+	// 	XMLConfigFileReader cfg(*this, fp);
+	// 	cfg.loadModel();
+}
+
+// Save
+func (mdl *Model) Save(configDir, configFile string) {
+	//fp := configDir + configFile;
+	//
+	//// LOG_DEBUG("Saving xml configuration: " << fp);
+	//// XMLConfigFileWriter cfg(*this, fp);
+	//cfg.saveModel();
+}
+
+// ParamIdx returns the index or -1 if not found
+func (mdl *Model) ParamIdx(nm string) int {
+	for i, p := range mdl.Params {
+		if p.Name == nm {
+			return i
+		}
+	}
+	return -1
+}
 
 //CategoryTry returns the address of the named Category or nil if not found
 func (mdl *Model) CategoryTry(nm string) *Category {
@@ -107,263 +103,169 @@ func (mdl *Model) PostureTry(nm string) *Posture {
 	return nil
 }
 
-//
-//
-////
-//func (mdl *Model) evalEquationFormula(const Equation& equation) const
-//{
-//	return equation.evalFormula(formulaCodebolList_);
-//}
-//
-////
-//func (mdl *Model) findEquationGroupName(const std::string& name) const
-//{
-//	for (const auto& item : equationGroupList_) {
-//		if (item.name == name) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findEquationName(const std::string& name) const
-//{
-//	for (const auto& group : equationGroupList_) {
-//		for (const auto& item : group.equationList) {
-//			if (item->name() == name) {
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findEquationIndex(const std::string& name, unsigned int& groupIndex, unsigned int& index) const
-//{
-//	for (unsigned int i = 0, groupListSize = equationGroupList_.size(); i < groupListSize; ++i) {
-//		const auto& group = equationGroupList_[i];
-//		for (unsigned int j = 0, size = group.equationList.size(); j < size; ++j) {
-//			if (group.equationList[j]->name() == name) {
-//				groupIndex = i;
-//				index = j;
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-//std::shared_ptr<Equation>
-//Model::findEquation(const std::string& name)
-//{
-//	for (const auto& group : equationGroupList_) {
-//		for (const auto& equation : group.equationList) {
-//			if (equation->name() == name) {
-//				return equation;
-//			}
-//		}
-//	}
-//	return std::shared_ptr<Equation>();
-//}
-//
-////
-//func (mdl *Model) getParameterMinimum(unsigned int parameterIndex) const
-//{
-//	if (parameterIndex >= parameterList_.size()) {
-//		THROW_EXCEPTION(InvalidParameterException, "Invalid parameter index: " << parameterIndex << '.');
-//	}
-//
-//	return parameterList_[parameterIndex].minimum();
-//}
-//
-////
-//func (mdl *Model) getParameterMaximum(unsigned int parameterIndex) const
-//{
-//	if (parameterIndex >= parameterList_.size()) {
-//		THROW_EXCEPTION(InvalidParameterException, "Invalid parameter index: " << parameterIndex << '.');
-//	}
-//
-//	return parameterList_[parameterIndex].maximum();
-//}
-//
-//const Parameter&
-//Model::getParameter(unsigned int parameterIndex) const
-//{
-//	if (parameterIndex >= parameterList_.size()) {
-//		THROW_EXCEPTION(InvalidParameterException, "Invalid parameter index: " << parameterIndex << '.');
-//	}
-//
-//	return parameterList_[parameterIndex];
-//}
-//
-////
-//func (mdl *Model) findParameterName(const std::string& name) const
-//{
-//	for (const auto& item : parameterList_) {
-//		if (item.name() == name) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findCodebolName(const std::string& name) const
-//{
-//	for (const auto& item : symbolList_) {
-//		if (item.name() == name) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-//
-// * Find a Transition with the given name.
-// *
-//// returns an empty shared_ptr if the Transition was not found.
-// */
-//const std::shared_ptr<Transition>
-//Model::findTransition(const std::string& name) const
-//{
-//	for (const auto& group : transitionGroupList_) {
-//		for (const auto& transition : group.transitionList) {
-//			if (transition->name() == name) {
-//				return transition;
-//			}
-//		}
-//	}
-//	return std::shared_ptr<Transition>();
-//}
-//
-////
-//func (mdl *Model) findTransitionGroupName(const std::string& name) const
-//{
-//	for (const auto& item : transitionGroupList_) {
-//		if (item.name == name) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findTransitionName(const std::string& name) const
-//{
-//	for (const auto& group : transitionGroupList_) {
-//		for (const auto& item : group.transitionList) {
-//			if (item->name() == name) {
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findTransitionIndex(const std::string& name, unsigned int& groupIndex, unsigned int& index) const
-//{
-//	for (unsigned int i = 0, groupListSize = transitionGroupList_.size(); i < groupListSize; ++i) {
-//		const auto& group = transitionGroupList_[i];
-//		for (unsigned int j = 0, size = group.transitionList.size(); j < size; ++j) {
-//			if (group.transitionList[j]->name() == name) {
-//				groupIndex = i;
-//				index = j;
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-// * Find a Special Transition with the given name.
-// *
-//// returns an empty shared_ptr if the Transition was not found.
-// */
-//const std::shared_ptr<Transition>
-//Model::findSpecialTransition(const std::string& name) const
-//{
-//	for (const auto& group : specialTransitionGroupList_) {
-//		for (const auto& transition : group.transitionList) {
-//			if (transition->name() == name) {
-//				return transition;
-//			}
-//		}
-//	}
-//	return std::shared_ptr<Transition>();
-//}
-//
-////
-//func (mdl *Model) findSpecialTransitionGroupName(const std::string& name) const
-//{
-//	for (const auto& item : specialTransitionGroupList_) {
-//		if (item.name == name) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findSpecialTransitionName(const std::string& name) const
-//{
-//	for (const auto& group : specialTransitionGroupList_) {
-//		for (const auto& item : group.transitionList) {
-//			if (item->name() == name) {
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-////
-//func (mdl *Model) findSpecialTransitionIndex(const std::string& name, unsigned int& groupIndex, unsigned int& index) const
-//{
-//	for (unsigned int i = 0, groupListSize = specialTransitionGroupList_.size(); i < groupListSize; ++i) {
-//		const auto& group = specialTransitionGroupList_[i];
-//		for (unsigned int j = 0, size = group.transitionList.size(); j < size; ++j) {
-//			if (group.transitionList[j]->name() == name) {
-//				groupIndex = i;
-//				index = j;
-//				return true;
-//			}
-//		}
-//	}
-//	return false;
-//}
-//
-// * Finds the first Rule that matches the given sequence of Postures.
-// */
-//const Rule*
-//Model::findFirstMatchingRule(const std::vector<const Posture*>& postureSequence, unsigned int& ruleIndex) const
-//{
-//	if (ruleList_.empty()) {
-//		ruleIndex = 0;
-//		return nullptr;
-//	}
-//
-//	unsigned int i = 0;
-//	for (const auto& r : ruleList_) {
-//		if (r->numberOfExpressions() <= postureSequence.size()) {
-//			if (r->evalBooleanExpression(postureSequence)) {
-//				ruleIndex = i;
-//				return r.get();
-//			}
-//		}
-//		++i;
-//	}
-//
-//	ruleIndex = 0;
-//	return nullptr;
-//}
-//
-//func (mdl *Model) PostureNameTry(nm string) *Posture {
-//   for _, p := range mdl.Postures {}
-//		if p.Name == name {
-//			return p
-//		}
-//	return nil
-//}
-//
+// EvalEquationFormula
+func (mdl *Model) EvalEquationFormul(eq *Equation) float64 {
+
+	return eq.Eval(mdl.Formula)
+}
+
+// FindEquationGroup
+func (mdl *Model) findEquationGroupTry(nm string) *EqGroup {
+	for _, eg := range mdl.EquationGrps {
+		if eg.Name == nm {
+			return &eg
+		}
+	}
+	return nil
+}
+
+// EquationIndexTry returns the group and equation index if name is found, otherwise -1, -1
+func (mdl *Model) EquationIndexTry(nm string) (grpIdx, eqIdx int) {
+	for i, grp := range mdl.EquationGrps {
+		for j, eq := range grp.Equations {
+			if eq.Name == nm {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+// EquationTry
+func (mdl *Model) EquationTry(nm string) *Equation {
+	for _, grp := range mdl.EquationGrps {
+		for _, eq := range grp.Equations {
+			if eq.Name == nm {
+				return &eq
+			}
+		}
+	}
+	return nil
+}
+
+// ParamMin returns the minimum parameter value
+func (mdl *Model) ParamMin(idx int) (float64, error) {
+	if idx >= len(mdl.Params) {
+		return 0.0, errors.New("Parameter index out of range")
+	}
+	return mdl.Params[idx].Min, nil
+}
+
+// ParamMax returns the minimum parameter value
+func (mdl *Model) ParamMax(idx int) (float64, error) {
+	if idx >= len(mdl.Params) {
+		return 0.0, errors.New("Parameter index out of range")
+	}
+	return mdl.Params[idx].Max, nil
+}
+
+// Param returns the parameter at given index
+func (mdl *Model) Param(idx int) (*Param, error) {
+	if idx >= len(mdl.Params) {
+		return nil, errors.New("Parameter index out of range")
+	}
+	return &mdl.Params[idx], nil
+}
+
+// ParamTry returns the address of the named Param or nil if not found
+func (mdl *Model) ParamTry(nm string) *Param {
+	for _, p := range mdl.Params {
+		if p.Name == nm {
+			return &p
+		}
+	}
+	return nil
+}
+
+// SymbolTry returns the address of the named Symbol or nil if not found
+func (mdl *Model) SymbolTry(nm string) *Symbol {
+	for _, s := range mdl.Symbols {
+		if s.Name == nm {
+			return &s
+		}
+	}
+	return nil
+}
+
+// TransitionTry returns the address of the named Transition or nil if not found
+func (mdl *Model) TransitionTry(nm string) *Transition {
+	for _, grp := range mdl.TransitionGrps {
+		for _, tr := range grp.Transitions {
+			if tr.Name == nm {
+				return &tr
+			}
+		}
+	}
+	return nil
+}
+
+// TransitionGrpTry returns the address of the named TransitionGroup or nil if not found
+func (mdl *Model) TransitionGroupTry(nm string) *TransGroup {
+	for _, grp := range mdl.TransitionGrps {
+		if grp.Name == nm {
+			return &grp
+		}
+	}
+	return nil
+}
+
+// TransitionGroupIndexTry returns the group and transition index if name is found, otherwise -1, -1
+func (mdl *Model) TransitionGroupIndexTry(nm string) (grpIdx, eqIdx int) {
+	for i, grp := range mdl.TransitionGrps {
+		for j, tr := range grp.Transitions {
+			if tr.Name == nm {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+// TransitionSpTry returns the address of the named special Transition or nil if not found
+func (mdl *Model) TransitionSpTry(nm string) *Transition {
+	for _, grp := range mdl.TransitionGrpsSp {
+		for _, tr := range grp.Transitions {
+			if tr.Name == nm {
+				return &tr
+			}
+		}
+	}
+	return nil
+}
+
+// TransitionGroupSpTry returns the address of the named special TransitionGroup or nil if not found
+func (mdl *Model) TransitionGroupSpTry(nm string) *TransGroup {
+	for _, grp := range mdl.TransitionGrpsSp {
+		if grp.Name == nm {
+			return &grp
+		}
+	}
+	return nil
+}
+
+// TransitionGroupSpIndexTry returns the special group and transition index if name is found, otherwise -1, -1
+func (mdl *Model) TransitionGroupSpIndexTry(nm string) (grpIdx, eqIdx int) {
+	for i, grp := range mdl.TransitionGrpsSp {
+		for j, tr := range grp.Transitions {
+			if tr.Name == nm {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
+}
+
+// FirstRule finds the first Rule that matches the given sequence of Postures. Returns rule and index
+func (mdl *Model) FirstRule(sequence []Posture) (*Rule, int) {
+	if len(mdl.Rules) == 0 {
+		return nil, -1
+	}
+	for i, r := range mdl.Rules {
+		if len(r.BoolExprs) <= len(sequence) {
+			if r.EvalExpr(sequence) {
+				return &r, i
+			}
+		}
+	}
+	return nil, 0
+}
